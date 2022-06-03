5b71e5899c60c4f2d1855cf074ea82d6050e9f0b
quynv86 <quynv86@gmail.com>
2022-06-03 23:28:27 +0700

    Welcome to Fis Java Training Course

--- fis-training-test01/src/main/java/vn/fis/training/CustomerManagementApplication.java
+++ fis-training-test01/src/main/java/vn/fis/training/CustomerManagementApplication.java
@@ -5,7 +5,7 @@ public class CustomerManagementApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to Fis Java Training Course!" );
     }
     // TODO: Implement method to test all CustomerService
 }
