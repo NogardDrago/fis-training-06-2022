e6ebbf2842cb87a720e869d2ee4c8b85dca4e786
quynv86 <quynv86@gmail.com>
2022-06-03 23:20:09 +0700

    Initialize Project For Fis-Trainning-Course June-2022

---
+++ fis-training-test01/README.md
@@ -0,0 +1,8 @@
+##### BAI TEST 01
+
+##### 01. Class Diagram
+
+
+
+##### 01. Yeu cau
+ Sinh viên implement cac method trong class SimpleCustomerService dựa trên đặc tả của interface CustomerService
---
+++ fis-training-test01/src/main/java/vn/fis/training/CustomerManagementApplication.java
@@ -0,0 +1,11 @@
+package vn.fis.training;
+
+
+public class CustomerManagementApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    // TODO: Implement method to test all CustomerService
+}
